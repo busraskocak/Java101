a537a71026b8ea76d964ad97145994bdb3e8cd47
Büşra <busraskocak@gmail.com>
2023-12-15 11:08:47 +0300

    Java101

--- Ucak Bilet Hesaplama/src/Main.java
+++ Ucak Bilet Hesaplama/src/Main.java
@@ -2,18 +2,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Java ile mesafeye ve şartlara göre uçak bileti fiyatı hesaplayan programı yapın. Kullanıcıdan Mesafe (KM), yaşı ve yolculuk tipi (Tek Yön, Gidiş-Dönüş) bilgilerini alın. Mesafe başına ücret 0,10 TL / km olarak alın. İlk olarak uçuşun toplam fiyatını hesaplayın ve sonrasında ki koşullara göre müşteriye aşağıdaki indirimleri uygulayın ;
-        //
-        //Kullanıcıdan alınan değerler geçerli (mesafe ve yaş değerleri pozitif sayı, yolculuk tipi ise 1 veya 2) olmalıdır. Aksi takdirde kullanıcıya "Hatalı Veri Girdiniz !" şeklinde bir uyarı verilmelidir.
-        //Kişi 12 yaşından küçükse bilet fiyatı üzerinden %50 indirim uygulanır.
-        //Kişi 12-24 yaşları arasında ise bilet fiyatı üzerinden %10 indirim uygulanır.
-        //Kişi 65 yaşından büyük ise bilet fiyatı üzerinden %30 indirim uygulanır.
-        //Kişi "Yolculuk Tipini" gidiş dönüş seçmiş ise bilet fiyatı üzerinden %20 indirim uygulanır.
-        int yas,km,yolculukTipi;
+        Scanner input = new Scanner(System.in);
 
-        Scanner= new Scanner(System.in);
+        System.out.print("Mesafe (KM): ");
+        int mesafe = input.nextInt();
 
-        System.out.println("Gideceğiniz Mesafeyi Giriniz:");
+        System.out.print("Yaşınız: ");
+        int yas = input.nextInt();
 
+        System.out.print("Yolculuk Tipi (1: Tek Yön, 2: Gidiş-Dönüş): ");
+        int yolculukTipi = input.nextInt();
+
+
+        if (mesafe <= 0 || yas <= 0 || (yolculukTipi != 1 && yolculukTipi != 2)) {
+            System.out.println("Hatalı Veri Girdiniz!");
+
+        }
+
+        double birimFiyat = 0.10;
+        double toplamFiyat = mesafe * birimFiyat;
+
+        // Yaş indirimleri
+        if (yas < 12) {
+            toplamFiyat *= 0.5;  // %50 indirim
+        } else if (yas >= 12 && yas <= 24) {
+            toplamFiyat *= 0.9;  // %10 indirim
+        } else if (yas >= 65) {
+            toplamFiyat *= 0.7;  // %30 indirim
+        }
+
+        // Yolculuk tipi indirimi
+        if (yolculukTipi == 2) {
+            toplamFiyat *= 0.8;  // %20 indirim
+        }
+
+        System.out.println("Toplam Tutar: " + toplamFiyat + " TL");
     }
 }
