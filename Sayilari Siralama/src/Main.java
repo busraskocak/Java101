bbfa31ffb6a147f7245086422f3ec3efb422e6e4
Büşra <busraskocak@gmail.com>
2023-12-15 09:53:26 +0300

    Sayıları Sıralama

--- Sayilari Siralama/src/Main.java
+++ Sayilari Siralama/src/Main.java
@@ -13,24 +13,20 @@ public class Main {
         System.out.print("3.Sayı:");
         c=input.nextInt();
 
-        if ((a<b)&& (a<c)){
-            if (b<c){
-                System.out.print("a<b<c");
-            }else {
-                System.out.println("a<c<b");
+        if ((a < b) && (a < c)) {
+            if (b < c) {
+                System.out.println("a < b < c");
+            } else {
+                System.out.println("a < c < b");
             }
-
-        } else if ((b<a) && (b<c)){
-                if (a<c){
-                    System.out.println("b<c<a");
-                }else{
-                    System.out.println("b<a<c");
-                }
-
-            }//YÜKLEMEDEN ÖNCE KONTROL ET İFLERİNDE SIKINTI VAR
-
+        } else if ((b < a) && (b < c)) {
+            if (a < c) {
+                System.out.println("b < c < a");
+            } else {
+                System.out.println("b < a < c");
+            }
+        } else if ((a == b) && (b == c)) {
+            System.out.println("Sayılar eşittir, sıralama yapılamaz.");
         }
-
     }
-
 }
