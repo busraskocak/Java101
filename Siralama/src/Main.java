d9dc768291d35f98f176651d98cde3a09cca92b1
Büşra <busraskocak@gmail.com>
2023-12-15 10:44:39 +0300

    Java101

--- Siralama/src/Main.java
+++ Siralama/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       int a,b,c;
+        int a,b,c;
 
         Scanner input = new Scanner(System.in);
 
